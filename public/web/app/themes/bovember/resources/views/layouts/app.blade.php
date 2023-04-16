647c7bd1e791fe714180c3ad6eee0aae8afea690
lljb3 <lljb3@lljb3.com>
2023-04-16 03:51:26 -0500

    Initial commit

---
+++ public/web/app/themes/bovember/resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<a class="sr-only sr-only-focusable visually-hidden" href="#main">
+  {{ __('Skip to content') }}
+</a>
+
+@include('sections.header')
+
+  <main id="main" class="main">
+    @yield('content')
+  </main>
+
+  @hasSection('sidebar')
+    <aside class="sidebar">
+      @yield('sidebar')
+    </aside>
+  @endif
